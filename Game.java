5e2e1d3e02ad3dcad67fea31aa13ac1adfcfb287
razkasyauqi08-cell <razkasyauqi08@gmail.com>
2026-04-13 10:39:49 +0700

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,1 +1,26 @@
+public void start() {
+  system.out.print("Masukkan Nama : ");
+  String name = input.nextLine();
 
+  player = new Player(name);
+
+  System.out.println("Hari pertama dimulai...");
+
+  //event
+  event.firstCustomer(player);
+
+  int choice;
+
+  do{
+    System.out.println("1.LANJUT JAGA TOKO");
+    System.out.println("2.EXIT");
+    System.out.println("Pilih : ");
+
+    choice = input.nextInt();
+    if (choice == 1) {
+        event.triggerEvent(player);
+    }
+
+  } while (choice != 0);
+  
+}
